3918f483d2cb34be5594205bec4784f82805b5b4
Gilmar <ti@arrastaosp.local>
2026-06-15 08:29:51 -0300

    fix: add cache buster to style.css to force clients to get the latest mobile fixes

--- index.php
+++ index.php
@@ -170,7 +170,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no">
     <title>Cetusg - <?= ucfirst($page) ?></title>
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= time() ?>">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <?php if (!empty($company['logo_url'])): ?>
         <link rel="icon" type="image/png" href="<?= htmlspecialchars($company['logo_url']) ?>">
